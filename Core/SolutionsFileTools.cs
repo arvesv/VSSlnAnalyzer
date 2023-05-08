270079489540b10f2c50e445ebb6ea9bb2e0a885
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2023-05-08 22:46:12 +0200

    Commandline

---
+++ Core/SolutionsFileTools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Core
+{
+    public class SolutionFileTools
+    {
+        public static bool Analyze(FileInfo solutionFile)
+        {
+            return false;
+        }
+
+    }
+}
---
+++ SlnAnalyze/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.CommandLine;
+
+
+var fileOption = new Option<FileInfo?>(
+    name: "--file",
+    description: "The solution file to analyze");
+
+var rootCommand =
+    new RootCommand(
+        "A tool that analyzes a Visual Studio SLN file and reports any project references in the SLN file that references outside of the SLN file.");
+ rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((file) => { Core.SolutionFileTools.Analyze(file!); }, fileOption);
+
+rootCommand.Invoke(args);
+
